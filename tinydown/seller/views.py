75e7b765c5f90237aa11a608296b6498c04f0188
CheeReus_11 <fanwei1995@hotmail.com>
2020-04-12 20:13:19 +0800

    走通简易的注册接口

--- tinydown/seller/views.py
+++ tinydown/seller/views.py
@@ -1,8 +1,10 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.template import loader
-
+import json
+from django.http import JsonResponse
 # Create your views here.
-from .models import Account
+from .models import Account, Seller
 
 def index(request):
     latest_account_list = Account.objects.order_by('-create_date')[:5]
@@ -11,3 +13,17 @@ def index(request):
         'latest_account_list': latest_account_list,
     }
     return render(request, 'seller/index.html', context)
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        data = json.loads(request.body) #取得数据
+        seller_name = data['username']
+        seller_password = data['password']
+        searchArray = Seller.objects.get_or_create(seller_name=seller_name, seller_password=seller_password) #尝试创建用户
+        print(searchArray)
+        print(data)
+        if searchArray[1] == True:
+            return JsonResponse({'result': 200, 'msg':'注册成功'})
+        else:
+            return JsonResponse({'result': 200, 'msg':'已有重复用户名'})
