3449119f0bf8f9caf365b019ff569e648fc80c6b
CheeReus_11 <fanwei1995@hotmail.com>
2020-04-12 16:56:54 +0800

    实现视图中的模板渲染

--- tinydown/seller/views.py
+++ tinydown/seller/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Account
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_account_list = Account.objects.order_by('-create_date')[:5]
+    template = loader.get_template('seller/index.html')
+    context = {
+        'latest_account_list': latest_account_list,
+    }
+    return render(request, 'seller/index.html', context)
